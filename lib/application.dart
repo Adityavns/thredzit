196ac1f764dc2a55ea2089a2fb3be016c297ec51
Pranay <pranay@ballebaaz.com>
2020-09-30 21:36:32 +0530

    Added App to firebase

---
+++ lib/application.dart
@@ -0,0 +1,13 @@
+class Application{
+    String name;
+    String guardianName;
+    String qualification;
+    String occupation;
+    String houseNumber;
+    String street;
+    String town;
+    String postOffice;
+    String policeStation;
+    String district;
+
+}
